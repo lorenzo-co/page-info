d633e0e4d9013cfddfa4e71527d6404be849cdfa
LorenzoCocciarelli <co.lorenzo@outlook.com>
2023-05-17 11:09:38 +0200

    feat: adds eslint, prettier and husky with relative configs

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest";
+
+describe("App test", () => {
+  it("should render", () => {
+    const result = 2 + 2;
+
+    expect(result).toEqual(4);
+  });
+});
